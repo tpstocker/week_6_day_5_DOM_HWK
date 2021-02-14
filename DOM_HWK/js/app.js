22429e4318671af0374006b0e52a683a06f9b91b
Tom Pies <tombo@Toms-MacBook-Pro.local>
2021-02-14 12:55:47 +0000

    mvp complete

--- DOM_HWK/js/app.js
+++ DOM_HWK/js/app.js
@@ -20,25 +20,26 @@ const createGameListItem = function(form) {
     const gameListItem = document.createElement('li');
     gameListItem.classList.add('game-list-item');
 
-    const gameTitle = document.createElement('h2');
+    const gameTitle = document.createElement('h3');
     gameTitle.textContent = form.gameTitle.value;
     gameListItem.appendChild(gameTitle);
 
-    const genre = document.createElement('h3');
+    const genre = document.createElement('p');
     genre.textContent = form.genre.value;
     gameListItem.appendChild(genre);
 
-    const platform = document.createElement('h3');
+    const platform = document.createElement('p');
     platform.textContent = form.platform.value;
     gameListItem.appendChild(platform);
 
-    const developer = document.createElement('h3');
+    const developer = document.createElement('p');
     developer.textContent = form.developer.value;
     gameListItem.appendChild(developer);
 
-    const releaseDate = document.createElement('h3');
+    const releaseDate = document.createElement('p');
     releaseDate.textContent = form.releaseDate.value;
     gameListItem.appendChild(releaseDate);
+
 
     return gameListItem;
 }
@@ -47,3 +48,4 @@ const handleDeleteAllClick = function(event) {
     const gameList = document.querySelector('#game-list');
     gameList.innerHTML = '';
   }
+
